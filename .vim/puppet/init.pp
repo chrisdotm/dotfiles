59eae11833340abf4163891e0de75b98299bd8f1
Chris McCoy <cmccoy@wayfair.com>
2014-03-06 10:58:18 -0500

    initial commit

---
+++ .vim/puppet/init.pp
@@ -0,0 +1,23 @@
+# === Class: classname
+#
+# description
+#
+# === Parameters
+#
+# None
+#
+# === Examples
+#
+# None
+#
+# === Authors
+#
+# Chris McCoy <cmccoy@wayfair.com>
+#
+# === Copyright
+#
+# Copyright 2013 Wayfair LLC.
+#
+class popp {
+
+}
